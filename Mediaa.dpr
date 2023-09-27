d9bf0cdc3cb9f1153ca53f5f7cb74df56320894d
Theo de Sá <98764883+jazznx@users.noreply.github.com>
2023-09-27 15:07:15 -0300

    Add files via upload

---
+++ Mediaa.dpr
@@ -0,0 +1,14 @@
+program Mediaa;
+
+uses
+  Vcl.Forms,
+  Media in 'Media.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
